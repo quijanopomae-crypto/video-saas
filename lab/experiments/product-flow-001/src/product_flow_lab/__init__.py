0ea805a3f26e6664e1567a8a9f909d49d1003e7a
quijanopomae-crypto <quijanopomae@gmail.com>
2026-09-20 12:05:48 -0500

    lab: add lab/experiments/product-flow-001/src/product_flow_lab/__init__.py

---
+++ lab/experiments/product-flow-001/src/product_flow_lab/__init__.py
@@ -0,0 +1,4 @@
+from product_flow_lab.contracts import ProductFlow, ProjectRequest
+from product_flow_lab.pipeline import run_product_flow
+
+__all__ = ["ProductFlow", "ProjectRequest", "run_product_flow"]
